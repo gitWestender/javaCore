e0f0696d6434c9d10b260d286abef9035d6e614d
Westender <javaxwestender@gmail.com>
2023-09-11 16:15:52 +0300

    Fix objects

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -6,21 +6,21 @@ public class Main {
     static Random rnd = new Random();
 
     public static void main(String[] args) {
-        Gryffindor harry = new Gryffindor("Гарри", "Поттер", getRND(), getRND(), getRND());
-        Gryffindor hermyona = new Gryffindor("гермиона", "Грейнджер", getRND(), getRND(), getRND());
-        Gryffindor ron = new Gryffindor("Рон", "Уизли", getRND(), getRND(), getRND());
+        Gryffindor harry = new Gryffindor("Гарри", "Поттер", getRND(), getRND(), getRND(), getRND(), getRND());
+        Gryffindor hermyona = new Gryffindor("гермиона", "Грейнджер", getRND(), getRND(), getRND(), getRND(), getRND());
+        Gryffindor ron = new Gryffindor("Рон", "Уизли", getRND(), getRND(), getRND(), getRND(), getRND());
 
-        Slytherin drako = new Slytherin("Драко", "Малфой", getRND(), getRND(), getRND(), getRND(), getRND());
-        Slytherin graham = new Slytherin("Грэхэм", "Монтегрю", getRND(), getRND(), getRND(), getRND(), getRND());
-        Slytherin gregory = new Slytherin("Грегори", "Гойл", getRND(), getRND(), getRND(), getRND(), getRND());
+        Slytherin drako = new Slytherin("Драко", "Малфой", getRND(), getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
+        Slytherin graham = new Slytherin("Грэхэм", "Монтегрю", getRND(), getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
+        Slytherin gregory = new Slytherin("Грегори", "Гойл", getRND(), getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
 
-        Hufflepuff zacharias = new Hufflepuff("Захария", "Смит", getRND(), getRND(), getRND());
-        Hufflepuff cedric = new Hufflepuff("Седрик", "Диггори", getRND(), getRND(), getRND());
-        Hufflepuff justin = new Hufflepuff("Джастин", "Финч-Флетчли", getRND(), getRND(), getRND());
+        Hufflepuff zacharias = new Hufflepuff("Захария", "Смит", getRND(), getRND(), getRND(), getRND(), getRND());
+        Hufflepuff cedric = new Hufflepuff("Седрик", "Диггори", getRND(), getRND(), getRND(), getRND(), getRND());
+        Hufflepuff justin = new Hufflepuff("Джастин", "Финч-Флетчли", getRND(), getRND(), getRND(), getRND(), getRND());
 
-        Ravenclaw cho = new Ravenclaw("Чжоу", "Чанг", getRND(), getRND(), getRND(), getRND());
-        Ravenclaw padme = new Ravenclaw("Падма", "Патил", getRND(), getRND(), getRND(), getRND());
-        Ravenclaw marcus = new Ravenclaw("Маркус", "Белби", getRND(), getRND(), getRND(), getRND());
+        Ravenclaw cho = new Ravenclaw("Чжоу", "Чанг", getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
+        Ravenclaw padme = new Ravenclaw("Падма", "Патил", getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
+        Ravenclaw marcus = new Ravenclaw("Маркус", "Белби", getRND(), getRND(), getRND(), getRND(), getRND(), getRND());
 
     }
 

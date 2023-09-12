738c2c8153a74777a9e2b85d5851780d37d12df3
Westender <javaxwestender@gmail.com>
2023-09-12 21:49:52 +0300

    Fully Change methods

--- src/lesson2/Hogwarts.java
+++ src/lesson2/Hogwarts.java
@@ -6,12 +6,15 @@ public class Hogwarts {
 
     static Random rnd = new Random();
 
+    protected String faculty;
+
     private String name;
     private String surname;
     private int magicPower;
     private int transgressionRange;
 
-    public Hogwarts(){}
+    public Hogwarts() {
+    }
 
     public Hogwarts(String name, String surname) {
         this.name = name;
@@ -50,60 +53,40 @@ public class Hogwarts {
         return rnd.nextInt(101);
     }
 
-    public int sumOfProperties() {
+    public int getSumOfMagicProperties() {
         return getMagicPower() + getTransgressionRange();
     }
 
-    public void gryffindorEquals(Gryffindor gr1, Gryffindor gr2) {
-        if (gr1.sumOfProperties() > gr2.sumOfProperties()) {
-            System.out.printf("\n%s(%d) лучший студент Гриффиндора, чем %s(%d)",
-                    gr1.getName(), gr1.sumOfProperties(), gr2.getName(), gr2.sumOfProperties());
+    public int getSumOfPersonalProperties() {
+        return 0;
+    }
+
+    public void studEquals(Hogwarts hg1, Hogwarts hg2) {
+        if (hg1.getClass() == hg2.getClass()) {
+            studEqualsOnPersonalProperties(hg1, hg2);
         } else {
-            System.out.printf("\n%s(%d) лучший студент Гриффиндора, чем %s(%d)",
-                    gr2.getName(), gr2.sumOfProperties(), gr1.getName(), gr1.sumOfProperties());
+            studEqualsOnMagicProperties(hg1, hg2);
         }
     }
 
-    public void slytherinEquals(Slytherin sl1, Slytherin sl2) {
-        if (sl1.sumOfProperties() > sl2.sumOfProperties()) {
-            System.out.printf("\n%s(%d) лучший студент Слизерина, чем %s(%d)",
-                    sl1.getName(), sl1.sumOfProperties(), sl2.getName(), sl2.sumOfProperties());
+    protected void studEqualsOnMagicProperties(Hogwarts hg1, Hogwarts hg2) {
+        if (hg1.getSumOfMagicProperties() > hg2.getSumOfMagicProperties()) {
+            System.out.printf("\n%s(%d) обладает большей мощностью, чем %s(%d)",
+                    hg1.getName(), hg1.getSumOfMagicProperties(), hg2.getName(), hg2.getSumOfMagicProperties());
         } else {
-            System.out.printf("\n%s(%d) лучший студент Слизерина, чем %s(%d)",
-                    sl2.getName(), sl2.sumOfProperties(), sl2.getName(), sl2.sumOfProperties());
+            System.out.printf("\n%s(%d) обладает большей мощностью, чем %s(%d)",
+                    hg2.getName(), hg2.getSumOfMagicProperties(), hg1.getName(), hg1.getSumOfMagicProperties());
         }
     }
 
-    public void hufflepufEquals(Hufflepuff hf1, Hufflepuff hf2) {
-        if (hf1.sumOfProperties() > hf2.sumOfProperties()) {
-            System.out.printf("\n%s(%d) лучший студент Пуффендуя, чем %s(%d)",
-                    hf1.getName(), hf1.sumOfProperties(), hf2.getName(), hf2.sumOfProperties());
+    protected void studEqualsOnPersonalProperties(Hogwarts hg1, Hogwarts hg2) {
+        if (hg1.getSumOfPersonalProperties() > hg2.getSumOfPersonalProperties()) {
+            System.out.printf("\n%s(%d) лучший студент %s, чем %s(%d)",
+                    hg1.getName(), hg1.getSumOfPersonalProperties(), hg1.faculty, hg2.getName(), hg2.getSumOfPersonalProperties());
         } else {
-            System.out.printf("\n%s(%d) лучший студент Пуффендуя, чем %s(%d)",
-                    hf2.getName(), hf2.sumOfProperties(), hf1.getName(), hf1.sumOfProperties());
+            System.out.printf("\n%s(%d) лучший студент %s, чем %s(%d)",
+                    hg2.getName(), hg2.getSumOfPersonalProperties(), hg2.faculty, hg1.getName(), hg1.getSumOfPersonalProperties());
         }
     }
-
-    public void ravenclawEquals(Ravenclaw rc1, Ravenclaw rc2) {
-        if (rc1.sumOfProperties() > rc2.sumOfProperties()) {
-            System.out.printf("\n%s(%d) лучший студент Когтеврана, чем %s(%d)",
-                    rc1.getName(), rc1.sumOfProperties(), rc2.getName(), rc2.sumOfProperties());
-        } else {
-            System.out.printf("\n%s(%d) лучший студент Когтеврана, чем %s(%d)",
-                    rc2.getName(), rc2.sumOfProperties(), rc1.getName(), rc1.sumOfProperties());
-        }
-    }
-
-    public static void studEquals(Hogwarts hg1, Hogwarts hg2) {
-        if (hg1.sumOfProperties() > hg2.sumOfProperties()) {
-            System.out.printf("\n%s(%d) лучший студент , чем %s(%d)",
-                    hg1.getName(), hg1.sumOfProperties(), hg2.getName(), hg2.sumOfProperties());
-        } else {
-            System.out.printf("\n%s(%d) лучший студент , чем %s(%d)",
-                    hg2.getName(), hg2.sumOfProperties(), hg1.getName(), hg1.sumOfProperties());
-        }
-    }
-
-
 
 }

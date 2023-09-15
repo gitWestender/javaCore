6a8c7e508e775e8da03f8f0c2cd9f9a6cc0dac6f
Westender <javaxwestender@gmail.com>
2023-09-15 10:52:54 +0300

    Rebuild class

--- src/lesson3/Car.java
+++ src/lesson3/Car.java
@@ -1,14 +1,17 @@
 package lesson3;
 
-public class Car {
+public class Car extends Vehicle{
 
-    public String modelName;
-    public int wheelsCount;
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
+    @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
 
+    @Override
     public void checkEngine() {
         System.out.println("Проверяем двигатель");
     }

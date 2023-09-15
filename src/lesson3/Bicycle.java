f2ac640470f92d55fd493745891310a4b402ecef
Westender <javaxwestender@gmail.com>
2023-09-15 10:27:51 +0300

    Start HW3

---
+++ src/lesson3/Bicycle.java
@@ -0,0 +1,10 @@
+package lesson3;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}

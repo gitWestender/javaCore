727dd6d683e8540dd16ddbf051a51a4ad5d9fe77
Westender <javaxwestender@gmail.com>
2023-10-16 20:15:47 +0300

    add task2

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -1,13 +1,15 @@
 package lesson7;
+
 import lesson7.tasks.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 10, 4, 5, 5, 6, 7));
 
     public static void main(String[] args) {
         Task1.printOddList(nums);
+        Task2.printEvenList(nums);
     }
 }

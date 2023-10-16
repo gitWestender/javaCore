c99b8ef7d49ea80fb88791e0d47d5e432d30aaef
Westender <javaxwestender@gmail.com>
2023-10-16 20:10:55 +0300

    add task1

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -1,4 +1,13 @@
 package lesson7;
+import lesson7.tasks.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+    public static void main(String[] args) {
+        Task1.printOddList(nums);
+    }
 }

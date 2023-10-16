146cad23cc16c73f5902519feae5137d980dbd93
Westender <javaxwestender@gmail.com>
2023-10-16 20:26:00 +0300

    add task3

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 public class Main {
     static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 10, 4, 5, 5, 6, 7));
+    static List<String> strings = new ArrayList<>(List.of("Привет", "как", "дела", "Привет", "дела", "хорошо"));
 
     public static void main(String[] args) {
         Task1.printOddList(nums);
         Task2.printEvenList(nums);
+        Task3.printUniqStrings(strings);
     }
 }
--- src/lesson7/tasks/Task3.java
+++ src/lesson7/tasks/Task3.java
@@ -1,4 +1,18 @@
 package lesson7.tasks;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Task3 {
+    public static void printUniqStrings(List<String> list) {
+        Set<String> setOfStrings = new HashSet<>();
+
+        for (String string : list) {
+            setOfStrings.add(string);
+        }
+
+        System.out.println(setOfStrings.toString());
+    }
 }

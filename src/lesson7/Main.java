412a032d3323d048acc0072d58d252fd794119ff
Westender <javaxwestender@gmail.com>
2023-10-18 19:21:01 +0300

    add task4 and complete

--- src/lesson7/Main.java
+++ src/lesson7/Main.java
@@ -8,10 +8,12 @@ import java.util.List;
 public class Main {
     static List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 10, 4, 5, 5, 6, 7));
     static List<String> strings = new ArrayList<>(List.of("Привет", "как", "дела", "Привет", "дела", "хорошо"));
+    static List<String> strs = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
     public static void main(String[] args) {
         Task1.printOddList(nums);
         Task2.printEvenList(nums);
         Task3.printUniqStrings(strings);
+        Task4.printNumsOfDoubles(strs);
     }
 }
--- src/lesson7/tasks/Task4.java
+++ src/lesson7/tasks/Task4.java
@@ -1,8 +1,21 @@
 package lesson7.tasks;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Task4 {
 
+    public static void printNumsOfDoubles(List<String> list) {
+        Map<String, Integer> mapOfDoubles = new HashMap<>();
+        Integer defaultValue = 1;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (mapOfDoubles.containsKey(list.get(i))) {
+                mapOfDoubles.put(list.get(i), mapOfDoubles.get(list.get(i)) + 1);
+            } else {
+                mapOfDoubles.put(list.get(i), defaultValue);
+            }
+        }
+
+        System.out.println(mapOfDoubles.toString());
+    }
 }
